7d0f37feba34944011854b51630433656d160832
Ira <sitronenelon@gmail.com>
2023-03-11 21:23:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+    public class Main {
+        public static void main(String[] args) {
+            int amountTicket = 28365;
+            int amountRuble = 20;
+
+            int amountMiles = (amountTicket / amountRuble);
+
+            System.out.println("Бонусные мили: " + amountMiles);
+        }
+}
